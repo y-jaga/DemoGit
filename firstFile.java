040c149b9a50967f254afcb966ef7b3b62f09ff3
y-jaga <yogeshjaga123@gmail.com>
2024-03-13 21:39:05 +0530

    first commit

---
+++ firstFile.java
@@ -0,0 +1,9 @@
+class Launch
+{
+	public static void main(String[] args)
+	{
+		System.out.println("hello yogesh, exploring git & github!");	
+
+	}
+
+}
---
+++ firstFileUpdated.java
@@ -0,0 +1,10 @@
+class Launch
+{
+	public static void main(String[] args)
+	{
+		System.out.println("hello yogesh, exploring git & github!");	
+		System.out.println("hello yogesh jaga");	
+
+	}
+
+}
